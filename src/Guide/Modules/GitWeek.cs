2ba58dd19f2f088e4171d33e8015443ed5026b49
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-29 22:18:43 +0000

    Refactor GitWeek (Refresh Score) Command
    This refactor now allows the Refresh Score command to only update user scores if it has changed from the last time the command was used. It's ugly but it works.

--- src/Guide/Modules/GitWeek.cs
+++ src/Guide/Modules/GitWeek.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -19,7 +20,7 @@ namespace Guide.Modules
             _gitweekStats = gitweekStats;
             _client = client;
         }
-        
+
         [Command("github register")]
         public async Task RegisterGitHub(string username)
         {
@@ -51,18 +52,48 @@ namespace Guide.Modules
             }
         }
 
-        [Command("refresh score")]
+        [Command("refresh score", RunMode = RunMode.Async)]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task RefreshScore()
         {
             try
             {
                 await ReplyAsync("I'm on it. It will take some time in order to not spam GitHub (and get destroyed by Microsoft as a result).\n\nOh and Discord will not be happy with me posting so much stuff at once, so I'll go checkout r/unixporn each time they rate limit me. We could switch our IP and pretend to be a different shard but that sounds like work.");
+
                 var tutorialGuild = _client.GetGuild(Constants.TutorialGuildId);
                 var channel = tutorialGuild.GetTextChannel(Constants.ScoreboardId);
-                var users = _gitweekStats.GetLeaderboards();
-                foreach(var user in users)
-                    await channel.SendMessageAsync(user);
+                var participents = _gitweekStats.GetParticipants();
+                var leaderboardMessages = await channel.GetMessagesAsync(participents.Count()).FlattenAsync();
+
+                foreach (var participent in participents)
+                {
+                    var rankString = _gitweekStats.GetStats(participent);
+                    var userRankMessage = leaderboardMessages.FirstOrDefault(x => x.Content.Contains(participent.GitHubUsername)) as IUserMessage;
+
+                    if (userRankMessage is null)
+                    {
+                        await channel.SendMessageAsync(rankString);
+                    }
+                    else
+                    {
+                        var warmupCommits = participent.CommitsByDate.Where(c => 
+                                    c.Key.Date >= _gitweekStats.WarmupStart.Date &&
+                                    c.Key.Date <= _gitweekStats.GitweekStart.Date)
+                                    .ToArray();
+
+                        var gitweekCommits = participent.CommitsByDate.Where(c =>
+                                    c.Key.Date > _gitweekStats.GitweekStart.Date)
+                                    .ToArray();
+
+
+                        if (!userRankMessage.Content.Contains($"{warmupCommits.Sum(c => c.Value)} commits")
+                            || !userRankMessage.Content.Contains($"{gitweekCommits.Sum(c => c.Value)} commits"))
+                        {
+                            await userRankMessage.ModifyAsync(x => x.Content = rankString);
+                        }
+                    }
+                }
+
             }
             catch (Exception e)
             {
