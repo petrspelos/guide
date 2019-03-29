3563d2e12042938e1b53fa80ddf715d4292dfb61
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-29 22:17:12 +0000

    Refactor GitWeekStats
    This refactor simply allows access to a few properties and the GetStats method.

--- src/Guide/GitWeek/IGitweekStats.cs
+++ src/Guide/GitWeek/IGitweekStats.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections.Generic;
 
 namespace Guide.GitWeek
 {
     public interface IGitweekStats
     {
+        DateTime WarmupStart { get; set; }
+        DateTime GitweekStart { get; set; }
         IEnumerable<GitweekParticipant> GetParticipants();
         string[] GetLeaderboards();
+        string GetStats(GitweekParticipant user);
     }
 }
