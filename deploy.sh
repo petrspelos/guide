d4d0b95a615aff08e3ead756d8d0ef6aacb8a208
petrspelos <petrspelos@users.noreply.github.com>
2019-11-16 18:49:44 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-cd ~/NetCore/Guide/src/Guide
+cd src/Guide
 dotnet publish -r linux-arm -c Release
-ssh pi@192.168.0.101 "pkill guide"
-scp -r ~/NetCore/Guide/src/Guide/bin/Release/netcoreapp2.1/linux-arm/publish/* pi@192.168.0.101:~/bots/Guide
-ssh pi@192.168.0.101 "~/bots/Guide/Guide &"
+cd bin/Release/netcoreapp2.2/linux-arm/publish/
+screen -S guide ./Guide
